571af17162352f01ffcb99696c4558ed47da0602
minjielu <minjielu199035@126.com>
2015-09-20 15:22:20 -0500

    hw 2 part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,7 +10,7 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
   end
 
-  def index
+  def index(:title, :release_date)
     @movies = Movie.order(:title, :release_date)
   end
 
