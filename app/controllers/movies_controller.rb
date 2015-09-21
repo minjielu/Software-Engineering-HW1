d08ea44a5f0c0cdd5984175879bb6c057da513d5
minjielu <minjielu199035@126.com>
2015-09-20 19:29:08 -0500

    hw 2 part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,7 +14,6 @@ class MoviesController < ApplicationController
     sort=params[:sort]
     if params[:ratings].present?
       @movies= Movie.where(rating: params[:ratings].keys).order(sort)
-      session[:ratings]=params[:ratings]
     elseif session[:ratings].present?
       @movies= Movie.where(rating: session[:ratings].keys).order(sort)
     else
